a3afd105ca26556b10dce5d9098ef985e15fff8b
cam330 <wilcoxcam@gmail.com>
2017-07-18 14:40:27 -0700

    Initial Commit

---
+++ beaconFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  beaconFinder
+//
+//  Created by Cameron Wilcox on 7/18/17.
+//  Copyright © 2017 Cameron Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
