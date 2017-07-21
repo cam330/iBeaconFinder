808b0f04b9a9f30c0d7a9de6b130b1d4547c94ee
cam330 <wilcoxcam@gmail.com>
2017-07-21 09:14:42 -0700

    begin map

--- beaconFinder/ViewController.swift
+++ beaconFinder/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     @IBOutlet var latLabel: UILabel!
     @IBOutlet var lngLabel: UILabel!
     @IBOutlet var distanceLabel: UILabel!
-    @IBOutlet var mapView: MKMapView!
+//    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var rssiDistanceLabel: UILabel!
     
     let locationManager = CLLocationManager()
     let region = CLBeaconRegion(proximityUUID: UUID(uuidString: "5122CBA8-9020-40B5-A252-AC84F6041A02")!, identifier: "Estimotes")
@@ -26,11 +27,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+
         
         locationManager.delegate = self
         if CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedWhenInUse {
+            
             locationManager.requestWhenInUseAuthorization()
         }
         locationManager.startRangingBeacons(in: region)
@@ -46,58 +47,71 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         let knownBeacons = beacons.filter{$0.proximity != CLProximity.unknown}
 
         let trimmed = String(format: "%.2", longitude)
-//        print(trimmed)
-        
-        
+        print(knownBeacons)
+        print("WOKRINO")
         
         if knownBeacons.count > 0 {
             let closestBeacon = knownBeacons[0] as CLBeacon
-//            print(closestBeacon)
+            print(closestBeacon)
             var lbl = closestBeacon.accuracy
             distanceLabel.text = String(format: "%.2f", arguments: [lbl])
             
             let latValue = Int(closestBeacon.major) * 2
             let lngValue = Int(closestBeacon.minor) * 2
             
+            let distFactor = Float(closestBeacon.rssi + 30) / -70
+            
+            rssiDistanceLabel.text = String(distFactor)
+            
+            
             latLabel.text = longitude + String(lngValue)
             lngLabel.text = latitude + String(latValue)
             
             annotation.coordinate = CLLocationCoordinate2D(latitude: Double(latitude + String(latValue))!, longitude: Double(longitude + String(lngValue))!)
-            mapView.addAnnotation(annotation)
+//            mapView.addAnnotation(annotation)
         }
         
     }
     
-    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-        
-        // get the particular pin that was tapped
-        let pinToZoomOn = view.annotation
-        
-        // optionally you can set your own boundaries of the zoom
-        let span = MKCoordinateSpanMake(0.1, 0.1)
-        
-        // or use the current map zoom and just center the map
-        // let span = mapView.region.span
-        
-        // now move the map
-        let region = MKCoordinateRegion(center: pinToZoomOn!.coordinate, span: span)
-        mapView.setRegion(region, animated: true)
-    }
+//    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+//        
+//        // get the particular pin that was tapped
+//        let pinToZoomOn = view.annotation
+//        
+//        // optionally you can set your own boundaries of the zoom
+//        let span = MKCoordinateSpanMake(0.1, 0.1)
+//        
+//        // or use the current map zoom and just center the map
+//        // let span = mapView.region.span
+//        
+//        // now move the map
+//        let region = MKCoordinateRegion(center: pinToZoomOn!.coordinate, span: span)
+//        mapView.setRegion(region, animated: true)
+//    }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
 //        let location = locations[0]
+    
         
+        let myLocation = locations.last!
+        
+        let center = CLLocationCoordinate2D(latitude: myLocation.coordinate.latitude, longitude: myLocation.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+//        self.mapView.setRegion(region, animated: true)
 
         
         let when = DispatchTime.now() + 1
         
         DispatchQueue.main.asyncAfter(deadline: when) {
+//            print(locations.first?.coordinate.longitude)
+//            print(locations.first?.coordinate.latitude)
             if let location = locations.first {
                 self.longitude = String(location.coordinate.longitude)
                 let lngRange: Range<String.Index> = self.longitude.range(of: ".")!
                 let lngIndex: Int = self.longitude.distance(from: self.longitude.startIndex, to: lngRange.lowerBound)
-                print(lngIndex)
+//                print(lngIndex)
                 
                 self.latitude = String(location.coordinate.latitude)
                 let latRange: Range<String.Index> = self.latitude.range(of: ".")!
@@ -107,8 +121,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
                 self.longitude = self.longitude.substring(to: lngTrimIndex)
                 let latTrimIndex = self.latitude.index(self.latitude.startIndex, offsetBy: latIndex+2)
                 self.latitude = self.latitude.substring(to: latTrimIndex)
-                print(self.longitude)
-                print(self.latitude)
+//                print(self.longitude)
+//                print(self.latitude)
             }
         }
         
